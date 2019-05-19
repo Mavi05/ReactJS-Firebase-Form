5597e12b146628d5169ea50aaceec13b51355717
Manish Mavi <manishmavi823@gmail.com>
2019-05-19 23:02:22 +0530

    Added Form and Linked to Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,11 +3,7 @@ import firebase from 'firebase';
 const config = {
     apiKey: "AIzaSyAAj83OWg74eUHaXcXriVYFklT5AZAC7m0",
     authDomain: "deevesoft-14fc0.firebaseapp.com",
-    databaseURL: "https://deevesoft-14fc0.firebaseio.com",
-    projectId: "deevesoft-14fc0",
-    storageBucket: "deevesoft-14fc0.appspot.com",
-    messagingSenderId: "647484469246",
-    appId: "1:647484469246:web:8a6fc60ec3474b8b"
+    databaseURL: "https://deevesoft-14fc0.firebaseio.com"
 };
 
 const firebaseConf = firebase.initializeApp(config);
